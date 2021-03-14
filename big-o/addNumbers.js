b6949962a53a8be92866e8a13c4b281a37d44227
karan <karan.bruellau1997@gmail.com>
2021-03-15 00:29:30 +0100

    moved script to own file outside html

--- big-o/addNumbers.js
+++ big-o/addNumbers.js
@@ -10,7 +10,12 @@ function addUpToV2(n){
     return n * (n + 1) / 2;
 }
 
-let time1 = performance.now();
-console.log(addUpToV1(1000000000));
-// var time2 = performance.now();
-// console.log(`time elapsed: ${(time2 - time1) / 1000} seconds`)
+var time1 = performance.now();
+addUpToV1(1000000000);
+var time2 = performance.now();
+console.log(`time elapsed v1: ${(time2 - time1) / 1000} seconds`)
+
+time1 = performance.now();
+addUpToV2(1000000000);
+time2 = performance.now();
+console.log(`time elapsed v2: ${(time2 - time1) / 1000} seconds`)
